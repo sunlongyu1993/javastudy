7484025414d7d3fa92fcc1dce187c7b65de4b7be
1127273738@qq.com <1127273738@qq.com>
2021-01-07 13:53:42 +0800

    封装读取properties文件类

--- src/main/java/com/testfan/MavenStudy/apistudy/HttpClient/CrmTestFZ.java
+++ src/main/java/com/testfan/MavenStudy/apistudy/HttpClient/CrmTestFZ.java
@@ -1,14 +1,17 @@
 package com.testfan.MavenStudy.apistudy.HttpClient;
 
 import com.testfan.MavenStudy.apistudy.common.MyHttpMethod;
+import com.testfan.utils.MyPropertisUtil;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author 孙珑瑜
@@ -16,27 +19,45 @@ import java.util.Map;
  */
 public class CrmTestFZ {
     String ip;
-    Map<Object,Object> param;
     Map<Object,Object> header;
     String token;
     @BeforeClass
     public void init(){
         ip = "http://192.168.23.129:8090";
-        param = new HashMap<>();
         header = new HashMap<>();
     }
 
     @Test
-    public void test001_login() throws IOException {
-        param.put("username", "admin");
-        param.put("password", "123456");
+    public void test001_login() throws Exception {
+//        Map<Object,Object> param =  param = new HashMap<>();
+//        param.put("username", "admin");
+//        param.put("password", "123456");
+        Map<Object, Object> param = MyPropertisUtil.getAll("src/main/resources/crmparams/crmlogin.properties");
         String response = MyHttpMethod.PostForm(ip + "/login", param, header);
         int statusCode = MyHttpMethod.getStatusCode();
         Assert.assertEquals(statusCode,200);
+        String code = JsonPath.from(response).getString("code");
+        Assert.assertEquals(code,"0");
+
         token = JsonPath.from(response).getString("Admin-Token");
+        header.put("Admin-Token",token);
     }
-    @Test
-    public void test002_addCustomer(){
+
+    /***
+     * 读取参数，参数json类型
+     * @throws IOException
+     */
+    @Test(dependsOnMethods = "test001_login")
+    public void test002_addCustomer() throws IOException {
+        JsonPath from = JsonPath.from(new File("src/main/resources/crmparams/addCustomer.json"));
+        String param = from.prettify();
+        String response = MyHttpMethod.PostJsonOrXml(ip + "/CrmCustomer/addOrUpdate", param, header);
+        System.out.println(response);
+        int statusCode = MyHttpMethod.getStatusCode();// http的响应状态码
+        Assert.assertEquals(statusCode,200);
+
+        String code = JsonPath.from(response).getString("code");//开发自定义的表示业务的code 字段
+        Assert.assertEquals(code,"0");
 
     }
 }
---
+++ src/main/java/com/testfan/utils/MyPropertisUtil.java
@@ -0,0 +1,57 @@
+package com.testfan.utils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * @author 孙珑瑜
+ * @version 20210107
+ */
+public class MyPropertisUtil {
+    /**
+     * 优化读取properties文件内容的脚本
+     * @return
+     */
+    public static Map<Object, Object> getAll(String filename) throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileReader(new File(filename)));
+        Map<Object,Object> prama= new HashMap<>();
+        Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
+        for (Map.Entry<Object, Object> entry:entrySet) {
+//            System.out.println(entry.getKey()+"="+entry.getValue());
+            prama.put(entry.getKey(),entry.getValue());
+        }
+        return prama;
+    }
+
+    public static Map<String, Object> name() throws Exception {
+        Properties properties= new Properties();
+        //load 表示要加载一个流，new FileReader 读取某一个文件，
+        properties.load(new FileReader(new File("src/main/resources/crmparams/crmlogin.properties")));
+       // properties中有内容，就可以将文件内容读出
+//        String username = properties.getProperty("username");
+//        String password = properties.getProperty("password");
+//        System.out.println(username);
+//        System.out.println(password);
+
+        Map<String,Object> prama= new HashMap<>();
+
+        // 读取所有的内容
+        Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
+        for (Map.Entry<Object, Object> entry:entrySet) {
+            System.out.println(entry.getKey()+"="+entry.getValue());
+
+            // 因为读取的内容是set,但是参数是map,需要将set转为map
+            prama.put(entry.getKey().toString(),entry.getValue());
+        }
+        return prama;
+    }
+
+    public static void main(String[] args) throws Exception {
+        name();
+    }
+}
