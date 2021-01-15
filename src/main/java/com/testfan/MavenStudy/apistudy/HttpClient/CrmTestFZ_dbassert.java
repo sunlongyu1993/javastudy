22f7423fd3950519bbe16efae3c459be467fa192
1127273738@qq.com <1127273738@qq.com>
2021-01-15 09:52:49 +0800

    数据库断言并封装数据库应用方法

--- src/main/java/com/testfan/MavenStudy/apistudy/HttpClient/CrmTestFZ_dbassert.java
+++ src/main/java/com/testfan/MavenStudy/apistudy/HttpClient/CrmTestFZ_dbassert.java
@@ -1,5 +1,6 @@
 package com.testfan.MavenStudy.apistudy.HttpClient;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONPath;
 import com.testfan.MavenStudy.apistudy.common.MyHttpMethod;
@@ -7,6 +8,7 @@ import com.testfan.MavenStudy.apistudy.utils.DButil;
 import com.testfan.MavenStudy.apistudy.utils.MyPropertisUtil;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -22,7 +24,7 @@ import java.util.Map;
  * @author 孙珑瑜
  * @version 20201231
  */
-public class CrmTestFZ_dataprovider {
+public class CrmTestFZ_dbassert {
     String ip;
     Map<Object,Object> header;
     String token;
@@ -32,17 +34,15 @@ public class CrmTestFZ_dataprovider {
     String dbpwd = "123456";
 
     @BeforeClass
-    public void init(){
+    public void init() throws SQLException {
         ip = "http://192.168.23.129:8090";
         header = new HashMap<>();
+        DButil.getconn(url,dbusername,dbpwd);
     }
 
     @DataProvider
     public Object[][] getCustomerData(){
         Object[][] obj={
-                {"客戶姓名为空","$.entity.customer_name",""},
-                {"mobile为空","$.entity.mobile",""},
-                {"website为空","$.entity.website",""},
                 {"数据库断言","$.entity.customer_name","数据库断言"}
         };
         return obj;
@@ -63,7 +63,7 @@ public class CrmTestFZ_dataprovider {
         header.put("Admin-Token",token);
     }
 
-    /***新增客户——客戶姓名为空
+    /***新增客户——数据库断言
      * 读取参数，参数json类型
      * @throws IOException
      */
@@ -83,50 +83,60 @@ public class CrmTestFZ_dataprovider {
 
         String code = JsonPath.from(response).getString("code");//开发自定义的表示业务的code 字段
         Assert.assertEquals(code,"0");
+
+        //数据库断言，对应新增客户，使用参数中的客户名称和数据库中最新的客户名称进行对比
+        String sql="SELECT customer_name FROM 72crm_crm_customer ORDER BY customer_id DESC LIMIT 1";
+//        DButil.getconn(url,dbusername,dbpwd);//获取数据库连接对象
+        List<Map<String, Object>> data = DButil.selectData(sql);//
+        String act_customer_name = data.get(0).get("customer_name").toString();
+//        String ext_customer_name= value;
+        Assert.assertEquals(act_customer_name,value);
+//        DButil.close();
     }
-    /***新增客户——mobile为空
+    /***客户列表查询接口——数据库断言
      * 读取参数，参数json类型
      * @throws IOException
      */
     @Test(dependsOnMethods = "test001_login",dataProvider = "getCustomerData")
-    public void test003_addCustomer(String casename,String path,String value) throws IOException {
-        JsonPath from = JsonPath.from(new File("src/main/resources/crmparams/addCustomer.json"));
-        String param = from.prettify();
-
-        //将参数进行转化
-        JSONObject jsonObject = JSONObject.parseObject(param);
-        //修改入参中的customer_name 为空
-        JSONPath.set(jsonObject,path,value);
-        //将替换后的jsonObject 重新赋值给param
-        param = jsonObject.toString();
-
-        String response = MyHttpMethod.PostJsonOrXml(ip + "/CrmCustomer/addOrUpdate", param, header);
-        System.out.println(response);
+    public void test003_queryPageList(String casename,String path,String value) throws Exception {
+        //读取参数
+        JsonPath from = JsonPath.from(new File("src/main/resources/crmparams/queryPageList.json"));
+        String param = from.prettify();//转化成字符串
+        int limit = from.getInt("limit");//从参数中取值来和sql 进行拼接
+        //请求接口
+        String response = MyHttpMethod.PostJsonOrXml(ip + "/CrmCustomer/queryPageList", param, header);
         int statusCode = MyHttpMethod.getStatusCode();// http的响应状态码
         Assert.assertEquals(statusCode,200);
-
         String code = JsonPath.from(response).getString("code");//开发自定义的表示业务的code 字段
         Assert.assertEquals(code,"0");
+        //从相应结果中获取到数据
+        //fastjson ，需要带$符号
+        JSONArray customerNames = (JSONArray) JSONPath.extract(response, "$.data.list[*].customerName");
+        JSONArray customerIds = (JSONArray) JSONPath.extract(response, "$.data.list[*].customerId");
+//        for (Object s:customerNames) {
+//            System.out.println(s);     }
+        //        数据库断言：查询接口如何进行断言——
+        String sql="SELECT customer_id,customer_name FROM 72crm_crm_customer ORDER BY create_time DESC limit "+limit;
+//        DButil.getconn(url,dbusername,dbpwd);//获取数据库连接对象
+        List<Map<String, Object>> data = DButil.selectData(sql);
+//        for (Map<String, Object> s :data) {
+//            System.out.println(s);}
+        Assert.assertEquals(data.size(),customerIds.size());// 对比数据库中查到的数据总数和返回值中的数据总数进行比较，如果一致再进行下列操作
+        for (int i = 0; i <data.size() ; i++) {
+            //数据库中查询的为预期的
+            Object ext_customer_name = data.get(i).get("customer_name");
+            Object ext_customer_id = data.get(i).get("customer_id");
+            //接口返回的为实际的
+            Object act_customer_id = customerIds.get(i);
+            Object act_customer_name = customerNames.get(i);
+            Assert.assertEquals(act_customer_id,ext_customer_id);
+            Assert.assertEquals(act_customer_name,ext_customer_name);
+        }
+//        DButil.close();
+
     }
-    /***新增客户——website为空
-     * 读取参数，参数json类型
-     * @throws IOException
-     */
-    @Test(dependsOnMethods = "test001_login",dataProvider = "getCustomerData")
-    public void test004_addCustomer(String casename,String path,String value) throws IOException {
-        JsonPath from = JsonPath.from(new File("src/main/resources/crmparams/addCustomer.json"));
-        String param = from.prettify();
-        JSONObject jsonObject = JSONObject.parseObject(param);
-        JSONPath.set(jsonObject,path,value);
-        param = jsonObject.toString();
-
-        String response = MyHttpMethod.PostJsonOrXml(ip + "/CrmCustomer/addOrUpdate", param, header);
-        System.out.println(response);
-        int statusCode = MyHttpMethod.getStatusCode();
-        Assert.assertEquals(statusCode,200);
-
-        String code = JsonPath.from(response).getString("code");
-        Assert.assertEquals(code,"0");
-
+    @AfterClass
+    public void afterclass() throws SQLException {
+        DButil.close();
     }
 }
--- src/main/java/com/testfan/MavenStudy/apistudy/utils/DButil.java
+++ src/main/java/com/testfan/MavenStudy/apistudy/utils/DButil.java
@@ -1,28 +1,155 @@
 package com.testfan.MavenStudy.apistudy.utils;
 
+import com.testfan.MavenStudy.apistudy.common.MyHttpMethod;
+
 import java.sql.*;
+import java.util.*;
+import java.util.logging.Logger;
+
+import static java.util.logging.Logger.*;
 
 /**
  * @author 孙珑瑜
  * @version 20210113
  */
 public class DButil {
-    //连接数据库
+    private static Connection conn;
+    private static Logger logger= Logger.getLogger(String.valueOf(DButil.class));
+
+
+    /**
+     * 获取数据库连接对象
+     * @param url
+     * @param dbusername
+     * @param dbpwd
+     * @throws SQLException
+     */
+    public static void getconn(String url,String dbusername, String dbpwd) throws SQLException {
+        conn = DriverManager.getConnection(url, dbusername, dbpwd);//连接数据库，返回连接对象
+    }
+
+    /**
+     * 查询方法
+     * @param sql
+     * @return
+     * @throws SQLException
+     */
+    public static List<Map<String, Object>> selectData(String sql) throws SQLException {
+        List<Map<String,Object >> result = new ArrayList<Map<String,Object >>();
+
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);//预处理sql语句对象
+        ResultSet resultSet = preparedStatement.executeQuery();//用来查询数据库信息
+        ResultSetMetaData metaData = resultSet.getMetaData();//metaData 是表单类的数据,可以获取列数，以及列的名称
+        int columnCount = metaData.getColumnCount();// 获取结果的总列数
+        while (resultSet.next()){// 如果有下一行，则一列列的读取
+             Map<String,Object > linemap = new HashMap<>();
+            for (int i = 1; i <=columnCount; i++) {
+                Object dbvaule = resultSet.getObject(i);//读这一行中的某一列的值
+                String columnLabel = metaData.getColumnLabel(i);// 获取列的名称
+                if(dbvaule ==null){
+                    dbvaule ="";
+                }
+
+//                System.out.println(columnLabel+"="+dbvaule.toString());
+                linemap.put(columnLabel,dbvaule);
+            }
+//            System.out.println("=====================================");
+            result.add(linemap);
+        }
+        return result;
+    }
+
+    /**
+     * Updata 可以执行新增、更新以及删除3中sql语句
+     * @param sql
+     * @return
+     */
+    public static int updataData(String sql)  {
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.executeQuery(sql);//用来查询数据库信息
+            int updateCount = stmt.getUpdateCount();
+            return updateCount;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            if (stmt != null){
+                try {
+                    stmt.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * close关闭连接池
+     * @throws SQLException
+     */
+    public static void close() throws SQLException {
+        conn.close();
+    }
+
+
+
+
     public static void main(String[] args) throws SQLException {
         String url ="jdbc:mysql://192.168.23.129:3306/crm?characterEncoding=utf8&useSSL=false&zeroDateTimeBehavior=convertToNull&tinyInt1isBit=false";
         String dbusername = "root";
         String dbpwd = "123456";
         String sql = "SELECT * FROM `72crm_crm_customer`";
+        List<Map<String, Object>> maps = selectData(sql);
+        for (Map<String, Object> q:maps
+             ) {
+            System.out.println(q);
+        }
 
-        Connection connection = DriverManager.getConnection(url, dbusername, dbpwd);//连接数据库，返回连接对象
-//        Statement statement = connection.createStatement();
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);//预处理sql语句对象
-        ResultSet resultSet = preparedStatement.executeQuery();//执行查询语句
-        ResultSetMetaData metaData = resultSet.getMetaData();
 
-        int columnCount = metaData.getColumnCount();// 获取结果的总列数
-        System.out.println(columnCount);
-        connection.close();//关闭连接
+//        Connection connection = DriverManager.getConnection(url, dbusername, dbpwd);//连接数据库，返回连接对象
+////        Statement statement = connection.createStatement();
+//        PreparedStatement preparedStatement = connection.prepareStatement(sql);//预处理sql语句对象
+//
+//        ResultSet resultSet = preparedStatement.executeQuery();//执行查询语句
+//        //metaData 是表单类的数据,可以获取列数，以及列的名称
+//        // customer_id =1,customer_name = hadaf
+//        ResultSetMetaData metaData = resultSet.getMetaData();
+//
+//        int columnCount = metaData.getColumnCount();// 获取结果的总列数
+//        System.out.println(columnCount);
+//        // 用循环遍历输出得到的数据
+//        while (resultSet.next()){// 如果有下一行，则一列列的读取
+//            for (int i = 1; i <columnCount ; i++) {
+//                Object object = resultSet.getObject(i);//读这一行中的某一列的值
+//                // 获取列的名称
+//                String columnLabel = metaData.getColumnLabel(i);
+//                if (object == null){
+//                    object = "";
+//                }
+//                System.out.println(columnLabel+"="+object.toString());
+//            }
+//            System.out.println("================================");
+//        }
+
+//        conn.close();//关闭连接
+        getconn(url,dbusername,dbpwd);
+        List<Map<String, Object>> data = selectData(sql);
+        for (Map<String,Object>  line:data) {
+//            System.out.println(line);// 將每一行都打印出来
+            // map 对象的遍历
+            Set<Map.Entry<String, Object>> entries = line.entrySet();
+            for (Map.Entry<String, Object> s:entries) {
+                System.out.println(s.getKey()+"="+s.getValue());
+            }
+
+
+        }
+        Object create_user_id = data.get(0).get("create_");
+        System.out.println(create_user_id.toString());
+
 
 
     }
