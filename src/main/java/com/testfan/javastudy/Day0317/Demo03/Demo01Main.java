929814639d674c4e6bd68fafe136a7c525b19058
sunlongyu <11Wn98f1927sly@@>
2021-03-18 14:17:36 +0800

    测试上传地址

--- src/main/java/com/testfan/javastudy/Day0317/Demo03/Demo01Main.java
+++ src/main/java/com/testfan/javastudy/Day0317/Demo03/Demo01Main.java
@@ -8,16 +8,17 @@ import org.testng.Assert;
  */
 public class Demo01Main {
     public static void main(String[] args) {
-//        Person person = new Person();
-//        int i = person.hashCode();
-//        System.out.println(i);
-//
-//        Person person2 = new Person();
-//        int i2 = person.hashCode();
-//        System.out.println(i2);
-//
-//        System.out.println(person.equals(person2));
+        Person person = new Person();
+        int i = person.hashCode();
+        System.out.println(i);
+
+        Person person2 = new Person();
+        int i2 = person.hashCode();
+        System.out.println(i2);
+
+        System.out.println(person.equals(person2));
         System.out.println("重地".hashCode());
         System.out.println("通话".hashCode());
+
     }
 }
