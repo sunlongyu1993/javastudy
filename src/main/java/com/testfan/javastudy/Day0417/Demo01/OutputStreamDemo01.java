10fa006232beaca64d7cc504f5d38d3cf6f154d9
sunlongyu <11Wn98f1927sly@@>
2021-04-18 22:04:04 +0800

    输出流写入多个字节

--- src/main/java/com/testfan/javastudy/Day0417/Demo01/OutputStreamDemo01.java
+++ src/main/java/com/testfan/javastudy/Day0417/Demo01/OutputStreamDemo01.java
@@ -7,9 +7,10 @@ import java.io.IOException;
 /**
  * @author 孙珑瑜
  * @version 20210417
+ * 字节输出流写入数据到文件
  */
 public class OutputStreamDemo01 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         try {
             FileOutputStream fos = new FileOutputStream("D:\\code\\slycode\\maven_project\\src\\main\\java\\com\\testfan\\javastudy\\Day0416\\Demo02\\1.txt");
             fos.write(97);
@@ -17,5 +18,6 @@ public class OutputStreamDemo01 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
--- src/main/java/com/testfan/javastudy/Day0417/Demo01/OutputStreamDemo02.java
+++ src/main/java/com/testfan/javastudy/Day0417/Demo01/OutputStreamDemo02.java
@@ -1,20 +1,33 @@
 package com.testfan.javastudy.Day0417.Demo01;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author 孙珑瑜
  * @version 20210417
+ * 字节输出流写多个字节
  */
 public class OutputStreamDemo02 {
-    public static void main(String[] args) {
-        try {
-            FileOutputStream fos = new FileOutputStream("D:\\code\\slycode\\maven_project\\src\\main\\java\\com\\testfan\\javastudy\\Day0416\\Demo02\\1.txt");
-            fos.write(97);
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("D:\\code\\slycode\\maven_project\\src\\main\\java\\com\\testfan\\javastudy\\Day0417\\Demo01\\1.txt");
+        fos.write(49);//在文件中写入100这个数子，需要3个字节
+        fos.write(48);
+        fos.write(48);
+        //
+        byte[] bytes = {65,66,67,68,69};
+//        byte[] bytes = {-65,-66,-67,68,69};
+        fos.write(bytes);
+
+        fos.write(bytes,1,2);
+
+        byte[] bytes1 = "你好".getBytes();
+        System.out.println(Arrays.toString(bytes1));
+        fos.write(bytes1);
+
+
+        fos.close();
     }
 }
