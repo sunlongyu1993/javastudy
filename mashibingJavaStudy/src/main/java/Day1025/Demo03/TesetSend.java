51aecdef7f9821d1b5a6743bc80d945d06c09bc1
sunlongyu <11Wn98f1927sly@@>
2021-11-01 21:55:34 +0800

    UDP异常处理以及完整通讯

--- mashibingJavaStudy/src/main/java/Day1025/Demo03/TesetSend.java
+++ mashibingJavaStudy/src/main/java/Day1025/Demo03/TesetSend.java
@@ -10,40 +10,58 @@ import java.util.Scanner;
  * 发送方
  */
 public class TesetSend {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         System.out.println("学生上线了");
-        DatagramSocket ds = new DatagramSocket(1234);//1、准备套接字,指定发送方的端口
-        //2、准备数据包
-        Scanner sc = new Scanner(System.in);//从键盘接收
-        System.out.println("学生：");
-        String str = sc.next();
+        DatagramSocket ds = null;//1、准备套接字,指定发送方的端口
+        try {
+            ds = new DatagramSocket(1234);
+            while (true){
+                //2、准备数据包
+                Scanner sc = new Scanner(System.in);//从键盘接收
+                System.out.print("学生说：");
+                String str = sc.next();
 //        String str = "老师，你好";
-        byte[] bytes = str.getBytes();
-        /**dgp：
-         * 第一个字段：传送数据转为字节数组
-         * 第二个字段：字节数组的长度
-         * 第三个字段：封装接收方的ip
-         * 第四个字段：指定接收方的端口号
-         */
-        DatagramPacket dgp = new DatagramPacket(bytes,bytes.length,
-                InetAddress.getByName("localhost"),2222);
-        //3、向老师发送消息
-        ds.send(dgp);
+                byte[] bytes = str.getBytes();
+                /**dgp：
+                 * 第一个字段：传送数据转为字节数组
+                 * 第二个字段：字节数组的长度
+                 * 第三个字段：封装接收方的ip
+                 * 第四个字段：指定接收方的端口号
+                 */
+                DatagramPacket dgp = new DatagramPacket(bytes,bytes.length,
+                        InetAddress.getByName("localhost"),2222);
+                //3、向老师发送消息
+                ds.send(dgp);
 
-        //4、接受老师的消息
-        //有一个空的数据包，用来接收老师传来的数据包
-        byte[] b = new byte[1024];
-        DatagramPacket dataStudent= new DatagramPacket(b,b.length);
-        //5、接受对方的数据包，然后放入我们的db数据包中填充
-        ds.receive(dataStudent);//接受完毕后，dataStudent 中就填充数据了
-        //6、取出数据
-        byte[] data = dataStudent.getData();
-        String strdata = new String(data,0,dataStudent.getLength());
-        System.out.println("老师对我说:"+strdata);
+                if (str.equals("byebye")){
+                    System.out.println("学生下线了。。。");
+                    break;
+                }
+
+                //4、接受老师的消息
+                //有一个空的数据包，用来接收老师传来的数据包
+                byte[] b = new byte[1024];
+                DatagramPacket dataStudent= new DatagramPacket(b,b.length);
+                //5、接受对方的数据包，然后放入我们的db数据包中填充
+                ds.receive(dataStudent);//接受完毕后，dataStudent 中就填充数据了
+                //6、取出数据
+                byte[] data = dataStudent.getData();
+                String strdata = new String(data,0,dataStudent.getLength());
+                System.out.println("老师对我说:"+strdata);
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            ds.close();
+        }
 
 
-        //关闭资源
-        ds.close();
+
 
 
     }
--- mashibingJavaStudy/src/main/java/Day1025/Demo03/TestReceive.java
+++ mashibingJavaStudy/src/main/java/Day1025/Demo03/TestReceive.java
@@ -1,9 +1,7 @@
 package Day1025.Demo03;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.*;
 import java.util.Scanner;
 
 /**
@@ -12,32 +10,49 @@ import java.util.Scanner;
  * 接收方
  */
 public class TestReceive {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         System.out.println("老师上线了：");
-        DatagramSocket dgs = new DatagramSocket(2222);//创建套接字，指定接收方的端口
-        //2、有一个空的数据包，用来接收 对方传来的数据包
-        byte[] b = new byte[1024];
-        DatagramPacket dgp= new DatagramPacket(b,b.length);
-        //3、接受对方的数据包，然后放入我们的db数据包中填充
-        dgs.receive(dgp);//接受完毕后，dgp 中就填充数据了
-        //4、取出数据
-        byte[] data = dgp.getData();
-        String str = new String(data);
-        System.out.println("学生说:"+str);
+        DatagramSocket dgs = null;//创建套接字，指定接收方的端口
+        try {
+            dgs = new DatagramSocket(2222);
+            while (true){
+                //2、有一个空的数据包，用来接收 对方传来的数据包
+                byte[] b = new byte[1024];
+                DatagramPacket dgp= new DatagramPacket(b,b.length);
+                //3、接受对方的数据包，然后放入我们的db数据包中填充
+                dgs.receive(dgp);//接受完毕后，dgp 中就填充数据了
+                //4、取出数据
+                byte[] data = dgp.getData();
+                String s = new String(data,0,dgp.getLength());
+                System.out.println("学生说:"+s);
+                if (s.equals("byebye")){
+                    System.out.println("学生下线，老师也要下线了。。。");
+                    break;
+                }
 
-        //老师发送消息
-        Scanner sc = new Scanner(System.in);
-        System.out.println("老师说：");
-        String strTeacher = sc.next();
+                //老师进行回复
+                Scanner sc = new Scanner(System.in);
+                System.out.print("老师说：");
+                String strTeacher = sc.next();
 //        String strTeacher = "同学,你好";
-        byte[] bytes = strTeacher.getBytes();
-        //封装数据，并且指定学生的ip 和端口号
-        DatagramPacket dpTeacher = new DatagramPacket(bytes,bytes.length,
-                InetAddress.getByName("localhost"),1234);
-        dgs.send(dpTeacher);
+                byte[] bytes = strTeacher.getBytes();
+                //封装数据，并且指定学生的ip 和端口号
+                DatagramPacket dpTeacher = new DatagramPacket(bytes,bytes.length,
+                        InetAddress.getByName("localhost"),1234);
+                dgs.send(dpTeacher);
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            // 5、关闭资源
+            dgs.close();
+        }
 
-        // 5、关闭资源
-        dgs.close();
+
 
 
     }
