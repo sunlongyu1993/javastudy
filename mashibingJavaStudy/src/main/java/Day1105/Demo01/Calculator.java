ddecfb6dfb157a920584a87824c8c44edb4d6e56
sunlongyu <11Wn98f1927sly@@>
2021-11-11 17:49:49 +0800

    junit5

---
+++ mashibingJavaStudy/src/main/java/Day1105/Demo01/Calculator.java
@@ -0,0 +1,14 @@
+package Day1105.Demo01;
+
+/**
+ * @author 孙珑瑜
+ * @version 211108
+ */
+public class Calculator {
+    public int add(int a,int b){
+      return a+b;
+    }
+    public int sub(int a,int b){
+        return a-b;
+    }
+}
---
+++ mashibingJavaStudy/src/test/java/Day1105/Demo01/TestTest.java
@@ -0,0 +1,25 @@
+package Day1105.Demo01;
+
+import junit.framework.TestCase;
+
+/**
+ * @author 孙珑瑜
+ */
+public class TestTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testInit() {
+    }
+
+    public void testClose() {
+    }
+
+    public void testTestadd() {
+    }
+}
